182288ec6f6a13111e5f3b3f73c81132cd17ad04
lldxck <895236755@qq.com>
2021-08-30 20:03:57 +0800

    实现slot

--- 复习文件夹/06.component/component/src/04.React实现slot/App.js
+++ 复习文件夹/06.component/component/src/04.React实现slot/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import NavBar from './NavBar'
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                app
+                {/* 
+                <NavBar>
+                    <div>返回</div>
+                    <div>title</div>
+                    <div>搜索</div>
+                </NavBar>
+                */}
+                <NavBar navLeft={<div>返回1</div>} navCenter={<div>title1</div>} navRight={<div>搜索1</div>} />
             </div>
         )
     }
